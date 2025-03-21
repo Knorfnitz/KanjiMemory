f7d671299cb48e530100215afe1d0538fd84d49b
Alexander Hasecke <knorfnitz@gmail.com>
2025-03-21 10:46:46 +0100

    buxfix with rotate

--- app/src/main/java/de/syntax_institut/projektwoche1/ui/components/GameCard.kt
+++ app/src/main/java/de/syntax_institut/projektwoche1/ui/components/GameCard.kt
@@ -20,13 +20,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
